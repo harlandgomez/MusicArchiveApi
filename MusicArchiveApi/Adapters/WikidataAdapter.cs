ee9cbaa59b066ac326bad16748b60bb728ce6e54
Harland Gomez <harlandgomez@gmail.com>
2019-04-08 12:06:41 +0200

    Removed httpClient variable and changed it with HttpClient base property.

--- MusicArchiveApi/Adapters/WikidataAdapter.cs
+++ MusicArchiveApi/Adapters/WikidataAdapter.cs
@@ -16,17 +16,13 @@ namespace MusicArchiveApi.Adapters
 
         public async Task<string> GetWikiDataTitleByQId(string qId)
         {
-            var httpClient = new HttpClient();
-            var title = string.Empty;
-            httpClient.DefaultRequestHeaders.Add("User-Agent", "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.1; WOW64; Trident/6.0;)");
-
             var wikiTitleQuery = new UriBuilder(UrlSettings.WikidataUrl)
             {
                 Query = $"action=wbgetentities&ids={qId}&format=json&props=sitelinks",
                 Port = NoPort
             };
 
-            var httpResponseMessage = httpClient.GetAsync(wikiTitleQuery.Uri).Result;
+            var httpResponseMessage = HttpClient.GetAsync(wikiTitleQuery.Uri).Result;
             if (!httpResponseMessage.IsSuccessStatusCode) return "Missing Title";
             var json = await httpResponseMessage.Content.ReadAsStringAsync();
             dynamic wikiDataResponse = JsonConvert.DeserializeObject(json);
